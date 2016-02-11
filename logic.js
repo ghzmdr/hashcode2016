22b924dd3ef4de7e149a4fb9bb47e0b5f786b9ba
Renato Formato <renato@rollstudio.it>
2016-02-11 21:14:23 +0100

    WIP

--- logic.js
+++ logic.js
@@ -1,6 +1,98 @@
 var ApplyLogic = function (modelData) {
+
 	//stuff
 	return // a list of unformatted operations
 }
+
+function getAvailableDrones() {
+
+}
+
+function getNearestDrones(pos) {
+
+}
+
+function getWarehousesForProductType(t) {
+
+}
+
+function getMaxLoad(drone, type, quantity) {
+
+	var pWeight = type.weight;
+
+	var weight = pWeight * quantity;
+
+	var maxQuantity;
+	if(weight <= drone.capacity) {
+		maxQuantity = quantity;
+	} else {
+		maxQuantity = Math.floor(drone.capacity/pWeight);
+	}
+
+	drone.capacity -= maxQuantity * pWeight;
+	return maxQuantity;	
+
+}
+
+function getOrderCost(order) {
+	var commands = [];
+	//while products to deliver
+	while(order.totalItems > 0) {
+		//for each product type
+		order.types.forEach(function(quantity, t) {
+
+			while(quantity > 0) {
+				//get warehouses
+				var w = getWarehousesForProductType(t);
+				
+				//get available drones
+				var drones = getAvailableDrones();
+
+				//for each avail drone
+				var minLoadCost = Number.POSITIVE_INFINITY;
+				var targetDrone = null;
+				var targetWarehouse = null;
+				//for each warehouse
+				w.forEach(function(warehouse, warehouseIndex) {
+					//for each avail drone
+					var nearest = getNearestDrones(warehouse.pos);
+
+					var loadCost = getDistance(nearest, warehouse.pos) + 1;
+
+					if(loadCost < minLoadCost) {
+						minLoadCost = loadCost;
+						targetDrone = nearest;
+						targetWarehouse = warehouseIndex;
+					}
+				})
+
+				var maxLoad = getMaxLoad(targetDrone, t, quantity);
+
+				quantity -= maxLoad;
+
+				targetDrone
+				commands.push({
+					drone: targetDrone,
+					type: 'L',
+					warehouse: targetWarehouse,
+					product: t,
+					quantity: maxLoad
+				});
+
+			}
+
+			order.totalItems -= quantity;
+
+
+			
+
+
+		}) 
+
+	}
+
+}
+
+
 
 module.exports = ApplyLogic
